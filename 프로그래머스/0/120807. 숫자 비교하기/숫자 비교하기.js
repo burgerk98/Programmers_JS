662db59c980087c56b98c18bb18274cf8a193895
강계령 <153696250+burgerk98@users.noreply.github.com>
2024-01-18 01:04:14 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,11 +1,10 @@
 function solution(num1, num2) {
     var answer;
-    if(0 <= num1 && num1 <= 10000 && 0 <= num2 && num2 <= 10000){
-        if(num1 === num2){
-            answer = 1;
-        } else{
-            answer = -1;
-        }
-        return answer;
+    if(num1 === num2){
+        answer = 1;
+    }else{
+        answer = -1;
     }
+    
+    return answer;
 }
