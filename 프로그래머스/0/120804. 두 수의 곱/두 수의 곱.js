6687552bac2b7c3b92cf9e571823d7daff0ad3da
강계령 <153696250+burgerk98@users.noreply.github.com>
2024-01-18 01:01:11 +0900

    [level 0] Title: 두 수의 곱, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.js
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.js
@@ -1,8 +1,4 @@
 function solution(num1, num2) {
-    var numOne = (0 <= num1 && num1 <= 100);
-    var numTwo = (0 <= num2 && num2 <= 100);
-    if(numOne && numTwo){
-        var answer = num1 * num2;
-        return answer;
-    }
+    var answer = num1 * num2
+    return answer;
 }
