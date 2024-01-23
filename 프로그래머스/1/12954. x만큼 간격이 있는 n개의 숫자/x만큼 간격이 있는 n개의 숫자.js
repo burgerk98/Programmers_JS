6b1b6a09bcca7a1b06fd83df151f20e8812f6d2c
강계령 <153696250+burgerk98@users.noreply.github.com>
2024-01-23 23:09:11 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.11 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
@@ -1,10 +1,10 @@
 function solution(x, n) {
     // 배열을 만들기 위해 초기화
     var answer = [];
-    // n개를 반환하기 위해 n만큼 반복
-    for(let i = 0; i < n; i++ ){
+    // n개를 반환하기 위해 n만큼 반복(자연수는 1부터)
+    for(let i = 1; i <= n; i++ ){
         //x부터 시작해 x씩 증가하는 숫자를 배열에 추가.
-        answer.push(x + x*i);
+        answer.push(x*i);
     }
     return answer;
 }
