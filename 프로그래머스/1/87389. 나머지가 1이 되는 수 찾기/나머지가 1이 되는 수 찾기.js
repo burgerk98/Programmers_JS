d6d7d63a77594667f2aad375ca3329c2a96201a7
강계령 <153696250+burgerk98@users.noreply.github.com>
2024-01-24 14:07:58 +0900

    Update 나머지가 1이 되는 수 찾기.js 주석추가

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -1,7 +1,9 @@
 function solution(n) {
     var answer = 0;
     var num = []
+    //x가 자연수라고 해 1부터 설정.
     for(let x = 1; x <= n; x++)
+        //n을 나눈 나머지가 1일 경우 num 배열에 추가하고 배열에서 작은 수를 리턴
         if(n % x === 1){
             num.push(x)
             answer = Math.min(...num)
