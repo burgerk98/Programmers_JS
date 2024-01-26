22ce2ecc0b9ec587314b2983df024ba2bf9858a7
강계령 <153696250+burgerk98@users.noreply.github.com>
2024-01-26 13:58:59 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -5,7 +5,7 @@ function solution(n) {
     
     // x의 제곱이 n이라면 x에 1을 더해 제곱하여 리턴, 아니면 -1을 리턴
     // 양의정수???ㅠㅠㅠ => isInteger을 사용해 정수인지, 그리고 양수인지 확인....
-    if(n === Math.pow(x,2) && Number.isInteger(x) && x > 0){
+    if(n === Math.pow(x,2) && Number.isInteger(x)){
         answer = Math.pow(x+1,2)
     }else{
         answer = -1
